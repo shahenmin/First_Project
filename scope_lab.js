6f0cbe92b31589ffd9d2a4e9b92376c28205fd36
shahenmin <you@example.comshahenmin@gmail.com>
2025-02-02 17:20:00 -0500

    commit

--- scope_lab.js
+++ scope_lab.js
@@ -1,5 +1,5 @@
 // Global scope
-var globalVar = "I'm a global variable";
+var globalVar = "I'm a new global variable";
 let globalLet = "I'm also global, but scoped with let";
 const globalConst = "I'm a global constant";
 
@@ -15,3 +15,18 @@ const blockConst = "I'm a block-scoped const";
 console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
+
+//Block Scope
+//console.log(blockVar);
+//console.log(blockLet);
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+    }
+    show();
+    
+    console.log(functionVar); // Throws ReferenceError
+    console.log(functionLet); // Throws ReferenceError
+    console.log(functionConst); // Throws ReferenceError
